0ac76382dc94e74f3e0ce11a25561464ba167c90
Sneeky Stryder <ifazilov3584@bths.edu>
2022-12-26 17:47:23 -0500

    done

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,6 +1,17 @@
 public class MainRunner {
     public static void main(String[] args) {
-        System.out.println(standardGreeting());
+        System.out.println(standardGreeting());             //K
+        System.out.println(ComputerLogic.getIdNum());       //K
+        ComputerLogic.newIdNum();                           //K
+        System.out.println(ComputerLogic.getIdNum());       //K
+        ComputerLogic.setGeneration();                      //K
+        System.out.println(ComputerLogic.getGeneration());  //K
+        System.out.println(ComputerLogic.codeNum());        //K
+
+        System.out.println(Contractor.contractorRequirements());    //L
+
+        Contractor contractor1 = new Contractor("Stryder", 24);
+        System.out.println(contractor1.contractorInfo());
 
     }
 
